99f69f044ba0506600156e667931c20b04664324
tysoccerow2316-svg <ty.soccer.ow.2316@gmail.com>
2026-03-01 11:11:20 +0900

    initial commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,12 @@ class CommentsController < ApplicationController
   def  create
 
     comment = Comment.new(comment_params)
-if comment.save
-  redirect_to prototype_path(comment.prototype)
-else
-  redirect_to prototype_path(comment.prototype)
-end
-end
+    if comment.save
+      redirect_to prototype_path(comment.prototype)
+    else
+      redirect_to prototype_path(comment.prototype)
+    end
+  end
 
 def comment_params
   params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,60 +1,61 @@
 class PrototypesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit, :delete]
-def  index
-  @prototypes = Prototype.all
-end
-
-def  new
-  @prototype = Prototype.new
-end
-
-def  create
-  @prototype = Prototype.new(prototype_params)
-   
-  if @prototype.save
-    redirect_to prototypes_path
-  else
-    render :new, status: :unprocessable_entity
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  def  index
+    @prototypes = Prototype.all
   end
-end
 
-def show 
-   @prototype = Prototype.find(params[:id])
-@comment = Comment.new
-@comments = @prototype.comments 
+  def  new
+    @prototype = Prototype.new
+  end
+
+  def  create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to prototypes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show 
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments 
   end
 
   def edit
-   @prototype = Prototype.find(params[:id])
-   if comment.user != current_user
-  redirect_to root_path 
+    @prototype = Prototype.find(params[:id])
+    before_action :move_to_index, except: [:index, :edit]
+def move_to_index
+unless user_signed_in? && prototype.user == current_user
+redirect_to action: :index
+end
 end
 
-def update
-  @prototype = Prototype.find(params[:id])
-  if @prototype.update(prototype_params)
-    redirect_to prototype_path(@prototype)
-  else
-    render :edit
+
   end
-end
 
-def destroy
-  @prototype = Prototype.find(params[:id]) 
-  @prototype.destroy 
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
 
-  redirect_to prototypes_path 
-end
-end
-
-
-
-private
-
-def prototype_params
-  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-end
+  def destroy
+    @prototype = Prototype.find(params[:id]) 
+    @prototype.destroy 
+    redirect_to prototypes_path 
+  end
 
 
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 
 end
